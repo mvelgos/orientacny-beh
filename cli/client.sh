b7ddc2b2a0245433427f96525e566df366af31dd
Miroslav Biňas <miroslav.binas@tuke.sk>
2019-08-05 23:47:20 +0200

    - nadpis kategorie je zarovnany na stred sirky tabulky - LINES je momentalne prevzaty z tput - refaktoring

--- cli/client.sh
+++ cli/client.sh
@@ -4,7 +4,7 @@ source progressbar.sh
 
 URL="https://raw.githubusercontent.com/bletvaska/orientacny-beh/master/data/example.data.csv"
 REPO="/tmp/data"
-HEADER="Por.;Meno;Klub;Kraj;Cas;Rozdiel"
+HEADER="Pos;Name;Organisation;Country;Time;Delta"
 DELAY=30
 CATEGORIES="M35-A M40-A M45-A M45-B M50-A M55-A M60-A M65-A M70-A M75-A W35-A W35-B W40-A W45-A W45-B W50-A W55-A W60-A W65-A W70-A"
 
@@ -16,6 +16,7 @@ if [[ ! -d ${REPO} ]]; then
     mkdir ${REPO}
 fi
 
+
 # superloop
 while true; do
     # download latest data first
@@ -26,12 +27,14 @@ while true; do
     fi
 
     # count nr of pages based on the number of lines on the screen and nr of lines in the file
-    LINES_PER_PAGE=$(( ${LINES} - 11 ))
+    lines=$(tput lines)
+    #columns=$(tput cols)
+    LINES_PER_PAGE=$(( ${lines} - 12 ))
 
     # cycle through categories
-    for CATEGORY in ${CATEGORIES}; do
+    for category in ${CATEGORIES}; do
         # extract specific category, remove first column, normalize line endings and append ';'
-        DATA=$(grep "^${CATEGORY};" data.csv | cut -d';' -f2- | sed -e 's/\r$//' -e 's/$/;/')
+        DATA=$(grep "^${category};" data.csv | cut -d';' -f2- | sed -e 's/\r$//' -e 's/$/;/')
 
         # count nr of pages
         PAGES=$(echo "$(echo "${DATA}" | wc -l) / ${LINES_PER_PAGE}" | bc -q)
@@ -39,8 +42,6 @@ while true; do
         # pagination
         for PAGE in $(seq 0 ${PAGES}); do
             clear
-            # print header first
-            figlet -c "${CATEGORY}"
 
             # extract lines in the range from START to STOP
             START=$(( ${PAGE} * ${LINES_PER_PAGE} + 1 ))
@@ -50,13 +51,18 @@ while true; do
             # combine header and content
             OUTPUT=$(echo -e "${HEADER}\n${CONTENT}" | column -t -s ";")
 
+            # print label first
+            width=$(echo "${OUTPUT}" | head -1 | wc -m)
+            echo
+            figlet -w ${width} -c "${category}"
+
             # insert separator as second line and print it
             separator=$(echo "${OUTPUT}" | head -1 | sed 's/[^|]/=/g')
             echo "${OUTPUT}" | sed "2i${separator}"
 
             # show progress bar
-            tput cup $(( ${LINES} - 2 )) 0
-            echo "Strana $(( ${PAGE} + 1 ))/$(( ${PAGES} + 1))"
+            tput cup $(( ${lines} - 2 )) 0
+            echo "Page $(( ${PAGE} + 1 )) of $(( ${PAGES} + 1))"
             progress-bar ${DELAY}
             echo -e "\r"
         done
