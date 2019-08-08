3b3cb30234e33063e8d6fb3aadc514fb8ad49ec4
Miroslav Biňas <miroslav.binas@tuke.sk>
2019-08-08 17:23:58 +0200

    upravene spustanie s tmux-om

--- cli/run.with.tmux.sh
+++ cli/run.with.tmux.sh
@@ -6,9 +6,9 @@ tmux start-server
 tmux new-session -d -s "${session}" -n 'session'
 
 tmux selectp -t 1
-tmux send-keys "./client.sh" C-m
+tmux send-keys "./client.sh config.men.sh" C-m
 
 tmux splitw -h -p 50
-tmux send-keys "./client.sh" C-m
+tmux send-keys "./client.sh config.women.sh" C-m
 
 tmux attach-session -t "${session}"
