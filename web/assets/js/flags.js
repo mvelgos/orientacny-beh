1aa55cd4777243c84463445145c33dd9dd76e2a5
Jaroslav Kovac <jaroslav.kovac24@gmail.com>
2019-08-16 08:49:47 +0200

    updated flags order

--- web/assets/js/flags.js
+++ web/assets/js/flags.js
@@ -3,8 +3,129 @@
 
 // flag dimensions: 64x64
 var flags = {
-    svk: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACRklEQVR42u3avUsbcRgH8Mz6F1QCQmkFQ0GIDaQ4CEpJF7OI1GDBIUJLoYR2cCgFJTQOIoKC4FAobYeIkA7qoLS0IDR9oUNih2aQVNpCHMzFRFNfSvvoc3AhL3fxEs/k7vINfLnj8gvH87mH33NDLERkaeRYAAAAAAAAAAAAAAAAAAAANNoHAAAAAAAAAAAAAKA6gL/hT/RnerbqGB6Ai0har8iGcTj7D0cV1/xPZ4wNkB17WhaAP3vee4prjsOfjQ2QGRgqKCjl7M6dHy6GCtYINrsYOSTdADS7X5LaBILREgCpID7++/W75FxufSX31DoXBqC2Y0wHsOvqy7W5tMvLfWdKAGm08TVO/h7A4Wlw8Oy5CGFKALUbWvFvTAPAT5wjdYIEkv/iw9dMC1Cc4j1A6V3BsACDE+/LvgipAdj5EjEugG3ktdjycoX9aL1G/UNz5Hq8RpN2+S7hibD08adxATiRyJZYSPTydRruekQPnPfFc8fNSWpyv8iFEeY7+qm3xy8eGYCnwt2ZD8YG4AIS/ilquzVTUPBZeeLwkhCLU4tnwdgAnPWNbWofCVUEEAhG6v70NQO44Vuh9W8JuuQJqir+duAdvXq7WffiNQOoBIGL546pd+srAgjWq1Rt0i43xb/GyOlbVmz7o9OpkbJ10nnuo2U0BeBwccerb8RipW7gUcjTIjse0E3hFwYgJTNwh4TvcYrGk7p76jUByO8IPRZeMwC9BwAAAAAAAAAAAAAAf5QEAAAAAAAAAAAAAAAAABozJ50GXC0kkBnlAAAAAElFTkSuQmCC",
+    // ad | andorra
+    and: "",
+
+    // ae | united arab emirates
+    are: "",
+
+    // af | afghanistan
+    afg: "",
+
+    // ag | antigua and barbuda
+    atg: "",
+
+    // ai | anguilla
+    aia: "",
+
+    // al | albania
+    alb: "",
+
+    // am | armenia
+    arm: "",
+
+    // an | netherlands antilles
+    ant: "",
+
+    // ao | angola
+    ago: "",
+
+    // aq | antarctica
+    ata: "",
+
+    // ar | argentina
+    arg: "",
+
+    // as | american samoa
+    asm: "",
+
+    // at | austria
+    aut: "",
+
+    // au | australia
+    aus: "",
+
+    // aw | aruba
+    abw: "",
+
+    // ax | aland islands ???
+    // missing iso3 code
+
+    // az | azerbaijan
+    aze: "",
+
+    // ba | bosnia and herzegovina
+    bih: "",
+
+    // bb | barbados
+    brb: "",
+
+    // bd | bangladesh
+    bdg: "",
+
+    // be | belgium
+    bel: "",
+
+    // bf | burkina faso
+    bfa: "",
+
+    // bg | bulgaria
+    bgr: "",
+
+    // bh | bahrain
+    bhr: "",
+
+    // bi | burundi
+    bdi: "",
+
+    // bj | benin
+    ben: "",
+
+    // bl | saint barthelemy
+    blm: "",
+
+    // bm | bermuda
+    bmu: "",
+
+    // bn | brunei darussalam
+    brn: "",
+
+    // bo | bolivia
+    bol: "",
+
+    // bq | bonaire, sint eustatius and saba
+    // missing iso3 code
+
+    // br | brasil
+    bra: "",
+
+    // bs | bahamas
+    bhs: "",
+
+    // bt | bhutan
+    btn: "",
+
+    // bv | bouvet island
+    // missing iso3 code
+
+    // bw | botswana
+    bwa: "",
+
+    // by | belarus
+    blr: "",
+
+    // bz | belize
+    blz: "",
+
+    // cz | czech republic
     cze: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAABOElEQVR42u3Sqw0CQRSF4W2BBEFwkGwJOAwCj0BDQhFIBMGgaYJgaYGEDqAIDAJ/waxAsOxjZu7rTHILmO8/GRFlns/15wEAAAAAAAAAAAAAAACAz612J3o8X+TofQN0phvK53s6X25+AYpzsobfAE7WUA7gYA3VAAyvoTqA0TXUBzC2hmYAhtbQDsDAGtoDKF9DOAClawgLUNxye9SyhjgAitYQD0DJGuIDCF9DGgDBa0gLIHAN6QGErYEPQMgaeAEGkzUd8jHdu32WYwWYjRZ07Q3ZPs8GwF2dFUBCdRYASdWTA0irngxAavUkAJKrRwXQUD0agJbqwQG0VQ8KoLF6EADN1VsDaK/eGMBK9UYAlqrXArBYvTKA1ep/AaxXLwXwUL0UwOsBAAAAAAAAAAAAAAAAAE7vDbJWLZQ3Zb5qAAAAAElFTkSuQmCC",
+
+    // gb | united kingdom
+    gbr: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACb0lEQVR42u2avUoDQRDHrxUCVnkSQdAmjYVtIBCwEHwAn8BnyAMEFBEs0hhT2ekTiGjjB2i0sNAUSSMoWIyZw5XNfbm7M3Mh3AwsKe643f39/5CZ2Y0AIKryiBSAAlAACkABKAAFoAAUgAKoOICndhu+RyOw4/X9AzZ3zyBa6bIME5fTabOGCa75VrdP4f5lApN+H67r9XgO/B33epACYB7iy8nontzCcuNwYQAsrR9A5/gmFhSFNd9/bDZtkWcBPDQafy9KuaEMAGs7gyLV7ZgFgBt863TgqlYDKTdIAnBUPY7R+DMNADe1P7iDr+EQpNwgBcBDdehfPEN94ygNwHwMNyblBm4Avqpv7Z3ba8kGgCPXDa0WyQ2cAEJUT6wlH4CUGzgAUFT3BsDtBioAqupBAELcgMCy3BAKgEt1EgAON4QA4FSdDIDqBh8AEqqzAQh1gysAKdVZAYS4wRWAlOoiAHzd4AJASvVCAFBi/AegpFAAqX5AlYYCUAAKQAEoAP0bVAA8qTCmpZieJgPTWDut9QUQ1wTTdNo8xzQb0+28fsNcagEsSH7bzDMLxwImr7Ps6wC7KsSCCwsv6jkFGYCr6rh4LG2xxHUFYCpMSTeQAPiojk0NbG74NkRMhSnlhiAAIapTW2JSbvAGEKo6R1NUwg3OAKiqc7bFOd3gBIBDde6DEdwUHsJQ3VAIgFN1qaMxqhtyAXCrLnk4SnFDCoCU6mUcj+e6weosJ92QAiClelkXJHzdUFgMcape9hUZVzdkApBQfR6XpFzckAKQpzpeNVvUa3JFbtCLkgpAASgABaAAFIACUAAKQAFUG8APtyfgJ+7gOvsAAAAASUVORK5CYII=",
+
+    // hu | hungary
     hun: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAc0lEQVR42u3QURGAQABCQSpYwj6msp2VuC9TsMy8AGzaZrnp8wAAAAAAAAAAAAAAtPmuu0sBAAAAAAAAAAAAAAAAwN/aAAAAAAAAAAAAAAAAAKDJ+3QpAAAAAAAAAAAAAAAAAACwHAAAAAAAAAAAAAAAox3PpCAtVEHDbQAAAABJRU5ErkJggg==",
-    gbr: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACb0lEQVR42u2avUoDQRDHrxUCVnkSQdAmjYVtIBCwEHwAn8BnyAMEFBEs0hhT2ekTiGjjB2i0sNAUSSMoWIyZw5XNfbm7M3Mh3AwsKe643f39/5CZ2Y0AIKryiBSAAlAACkABKAAFoAAUgAKoOICndhu+RyOw4/X9AzZ3zyBa6bIME5fTabOGCa75VrdP4f5lApN+H67r9XgO/B33epACYB7iy8nontzCcuNwYQAsrR9A5/gmFhSFNd9/bDZtkWcBPDQafy9KuaEMAGs7gyLV7ZgFgBt863TgqlYDKTdIAnBUPY7R+DMNADe1P7iDr+EQpNwgBcBDdehfPEN94ygNwHwMNyblBm4Avqpv7Z3ba8kGgCPXDa0WyQ2cAEJUT6wlH4CUGzgAUFT3BsDtBioAqupBAELcgMCy3BAKgEt1EgAON4QA4FSdDIDqBh8AEqqzAQh1gysAKdVZAYS4wRWAlOoiAHzd4AJASvVCAFBi/AegpFAAqX5AlYYCUAAKQAEoAP0bVAA8qTCmpZieJgPTWDut9QUQ1wTTdNo8xzQb0+28fsNcagEsSH7bzDMLxwImr7Ps6wC7KsSCCwsv6jkFGYCr6rh4LG2xxHUFYCpMSTeQAPiojk0NbG74NkRMhSnlhiAAIapTW2JSbvAGEKo6R1NUwg3OAKiqc7bFOd3gBIBDde6DEdwUHsJQ3VAIgFN1qaMxqhtyAXCrLnk4SnFDCoCU6mUcj+e6weosJ92QAiClelkXJHzdUFgMcape9hUZVzdkApBQfR6XpFzckAKQpzpeNVvUa3JFbtCLkgpAASgABaAAFIACUAAKQAFUG8APtyfgJ+7gOvsAAAAASUVORK5CYII="
+
+    // sk | slovakia
+    svk: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACRklEQVR42u3avUsbcRgH8Mz6F1QCQmkFQ0GIDaQ4CEpJF7OI1GDBIUJLoYR2cCgFJTQOIoKC4FAobYeIkA7qoLS0IDR9oUNih2aQVNpCHMzFRFNfSvvoc3AhL3fxEs/k7vINfLnj8gvH87mH33NDLERkaeRYAAAAAAAAAAAAAAAAAAAANNoHAAAAAAAAAAAAAKA6gL/hT/RnerbqGB6Ai0har8iGcTj7D0cV1/xPZ4wNkB17WhaAP3vee4prjsOfjQ2QGRgqKCjl7M6dHy6GCtYINrsYOSTdADS7X5LaBILREgCpID7++/W75FxufSX31DoXBqC2Y0wHsOvqy7W5tMvLfWdKAGm08TVO/h7A4Wlw8Oy5CGFKALUbWvFvTAPAT5wjdYIEkv/iw9dMC1Cc4j1A6V3BsACDE+/LvgipAdj5EjEugG3ktdjycoX9aL1G/UNz5Hq8RpN2+S7hibD08adxATiRyJZYSPTydRruekQPnPfFc8fNSWpyv8iFEeY7+qm3xy8eGYCnwt2ZD8YG4AIS/ilquzVTUPBZeeLwkhCLU4tnwdgAnPWNbWofCVUEEAhG6v70NQO44Vuh9W8JuuQJqir+duAdvXq7WffiNQOoBIGL546pd+srAgjWq1Rt0i43xb/GyOlbVmz7o9OpkbJ10nnuo2U0BeBwccerb8RipW7gUcjTIjse0E3hFwYgJTNwh4TvcYrGk7p76jUByO8IPRZeMwC9BwAAAAAAAAAAAAAAf5QEAAAAAAAAAAAAAAAAABozJ50GXC0kkBnlAAAAAElFTkSuQmCC",
 }
